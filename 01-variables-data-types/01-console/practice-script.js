e907ba97c0d6e425b9f23a3d6b1f1f73893333aa
Roderick Fitzgerald <liber6schol@outlook.com>
2025-10-09 19:01:14 -0400

    Updates to js and html within variables and data types

---
+++ 01-variables-data-types/01-console/practice-script.js
@@ -0,0 +1,27 @@
+console.log(100);
+
+console.log("Carpe Diem");
+
+console.log(35, 'Hola!', true);
+
+const x = 1000;
+
+console.log(x);
+
+console.error('Alert');
+
+console.warn('Warning');
+
+console.table({name: 'Roderick', email: 'liber6schol@outlook.com' });
+/*
+console.group('simple');
+console.log(x);
+console.error('Alert');
+console.warn('Warning');
+console.groupEnd();
+*/
+
+const styles = 'padding: 10px; background-color: grey; color: blue';
+console.log('%cHello World', styles);
+
+// "Ctrl + /" for a single line of code
