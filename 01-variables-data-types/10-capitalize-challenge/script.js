2ec9ad58e4dd906e0aa9ba30d1856e4d3598abb6
Roderick Fitzgerald <liber6schol@outlook.com>
2025-10-29 00:23:58 -0400

    Made changes to challenge in variables and data-types to practice more changes to properties using methods.

--- 01-variables-data-types/10-capitalize-challenge/script.js
+++ 01-variables-data-types/10-capitalize-challenge/script.js
@@ -1,12 +1,12 @@
 // Create a new string called "myNewString" that holds the value of "Developer", using the lowercase value from "myString"
-const myString = 'developer';
+const myString = 'omnipresent';
 
 let myNewString;
 
 // Solution 1:
 //myNewString = myString.charAt(0).toUpperCase() + myString.substring(1);
 // Solution 2:
-myNewString = myString[0].toUpperCase() + myString.substring(1);
+myNewString = myString[0].toUpperCase() + myString.slice(1);
 // Solution 3:
 //myNewString = `${myString[0].toUpperCase()}${myString.slice(1)}`;
 
