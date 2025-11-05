3a2a4e46a1dfd8e9d5b71bc7e9cb237f85501530
Roderick Fitzgerald <liber6schol@outlook.com>
2025-11-05 03:33:31 -0500

    More tests with array methods and nesting in the array challenge.

--- 02-arrays-and-objects/02-array-methods/script.js
+++ 02-arrays-and-objects/02-array-methods/script.js
@@ -15,7 +15,7 @@ arr.unshift(99);
 arr.shift();
 
 // reverse() - Reverse an array
-arr.reverse();
+//arr.reverse();
 
 // includes() - Check to see if something is in the array
 x = arr.includes(563);
@@ -27,18 +27,14 @@ x = arr.indexOf(28);
 x = arr.toString();
 x = arr.join();
 
-/*
-
 // slice() returns selected elements in an array, as a new array. Slice takes in the index of the first element and the index of the last element to be included in the new array.
 x = arr.slice(1, 4);
 
 // splice() works like slice() except it takes the index of the first element and the number of elements after that as a second argument. it also mutates the original array where slice() does not
-x = arr.splice(1, 4);
+//x = arr.splice(1, 4);
 
 // Remove a single element/value - The following will mutate the original array by taking out the element with the index of 4. x will be equal to a new array with that plucked out value.
-x = arr.splice(4, 1);
-
-*/
+//x = arr.splice(4, 1);
 
 // Chaining methods - Some methods can be chained depending on the return value.
 x = arr.splice(1, 4).reverse().toString().charAt(0);
--- 02-arrays-and-objects/04-array-challenge/script.js
+++ 02-arrays-and-objects/04-array-challenge/script.js
@@ -19,7 +19,7 @@ const arr1 = [1, 2, 3, 4, 5];
 const arr2 = [5, 6, 7, 8, 9, 10];
 
 // Solution 1
-const arr3 = arr1.slice(0, 4).concat(arr2);
+const arr3 = arr2.slice(1, 0).concat(arr1);
 
 // Solution 2
 const arr4 = [...arr1, ...arr2];
